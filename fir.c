345c3d26226ee2e5547211cb02780055f1246e14
materpiecepro <yipr6804@korea.ac.kr>
2023-03-22 21:50:02 +0900

    Initial commit

---
+++ fir.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
+
+#include <stdio.h>
+
+int main()
+{
+	double a;
+	double b;
+	double c;
+	printf("Å°, ¸ö¹«°Ô¸¦ ÀÔ·ÂÇÏ¼¼¿ä : ");
+	scanf(" %lf %lf", &a, &b);
+	printf("Æò±Õ ¸ö¹«°Ô : %f * %f * 22 / 10000 = %.2f\n", a, a, a * a * 22 / 10000);
+	printf("³» ¸ö¹«°Ô : %.2lf", b);
+}
